c51c06e580b897f50d294d567a0268b339977e1e
ortega-pablo <p.ortegariera@gmail.com>
2023-05-21 12:22:33 -0300

    feat: Agrego Eslint y Prettier a la configuración

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Glikad App</Text>
       <StatusBar style="auto" />
     </View>
   );
